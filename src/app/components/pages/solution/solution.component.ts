c2da41e8598f8ae019cc470589b138c53cdfacd7
Alex Carvalho do Nascimento <alex.pardoner@gmail.com>
2023-07-19 10:07:55 -0300

    version 190720231007

--- src/app/components/pages/solution/solution.component.ts
+++ src/app/components/pages/solution/solution.component.ts
@@ -3,8 +3,42 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-solution',
   templateUrl: './solution.component.html',
-  styleUrls: ['./solution.component.css']
+  styleUrls: ['./solution.component.css'],
 })
 export class SolutionComponent {
+  validate: string = '';
+  isValid: boolean = false;
 
+  validateTest(): boolean {
+    const colchetes = this.validate;
+    const colchetesValidosRegex = /^[(){}\[\]]+$/;
+
+    if (!colchetesValidosRegex.test(colchetes)) {
+      return false;
+    }
+
+    const sequencia: string[] = [];
+    const compatibilidade: { [key: string]: string } = {
+      ')': '(',
+      '}': '{',
+      ']': '[',
+    };
+
+    for (let colchete of colchetes) {
+      if (['(', '{', '['].includes(colchete)) {
+        sequencia.push(colchete);
+      } else {
+        const ultimoColchete = sequencia.pop();
+        if (ultimoColchete !== compatibilidade[colchete]) {
+          return false;
+        }
+      }
+    }
+
+    return sequencia.length === 0;
+  }
+
+  handleChange(): void {
+    this.isValid = this.validateTest();
+  }
 }
